ee1615c608e511b3bafc2029afb35abe30ffc498
Gil Tayar <gil@tayar.org>
2022-04-02 08:03:02 +0300

    wip

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# Typed JavaScript? For Real?
 
+A companion repo for my
+"Typed JavaScript? For real? The “type annotations” proposal and what it’s all about" talk.
---
+++ samples.ts
@@ -0,0 +1,19 @@
+//@ts-nocheck
+
+function add1(a, b) {
+  return a + b
+}
+
+function add2(a: number, b: number): number {
+
+}
+
+function add3(a: hi mom (I'm back!), b: (yes,siree)) {
+
+}
+
+type Foo = [[[<<<<<Foo!>>>>>]]]
+
+function getFoobs(): Foo[<!!>] {
+
+}
